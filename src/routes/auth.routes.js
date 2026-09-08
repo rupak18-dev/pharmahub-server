95883b373c90275a050ebd6ecb7b8ef922e50e65
harsha31dev <harsha31.dev@gmail.com>
2026-09-08 23:02:29 +0530

    Fix lint: remove unused userSchemas import in auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { validate } from "../middlewares/validate.js";
 import { auth } from "../middlewares/auth.js";
-import { authSchemas, userSchemas } from "../types/index.js";
+import { authSchemas } from "../types/index.js";
 import * as authController from "../controllers/auth.controller.js";
 
 const router = Router();
