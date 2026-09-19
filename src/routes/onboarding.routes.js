c28425b20335cfb66675e878fb86fb834bee7760
SreejaSri-Nallakula <nallakulasreejasri@gmail.com>
2026-09-19 21:03:59 +0530

    Backend Bug

--- src/routes/onboarding.routes.js
+++ src/routes/onboarding.routes.js
@@ -1,18 +1,5 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.js";
-<<<<<<< HEAD
-import { ok } from "../core/responses.js";
-
-const router = Router();
-
-router.get("/", auth, (req, res) => {
-  return ok(res, {}, "Onboarding data");
-});
-
-router.put("/", auth, (req, res) => {
-  return ok(res, req.body, "Onboarding data saved");
-});
-=======
 import { get, save } from "../controllers/onboarding.controller.js";
 
 // All onboarding persistence flows through the controller so completion-time
@@ -21,6 +8,5 @@ const router = Router();
 
 router.get("/", auth, get);
 router.put("/", auth, save);
->>>>>>> c876e587860aeb70c959d2e9fa3874741ecda6d3
 
 export default router;
